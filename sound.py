86dafbda86d4d725900f5aa8256559335fd37789
quangchuyen169 <114273374+quangchuyen169@users.noreply.github.com>
2024-01-09 21:20:39 +0700

    Update sound.py

--- sound.py
+++ sound.py
@@ -1,8 +1,7 @@
-import pyaudio
+import sounddevice as sd
+import numpy as np
 import wave
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 RECORD_SECONDS = 5
@@ -10,30 +9,19 @@ WAVE_OUTPUT_FILENAME = "record.wav"
 
 
 def record():
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
     print("* Recording")
 
-    frames = []
+    # Ghi âm và lưu dữ liệu vào mảng numpy
+    recording_data = sd.rec(int(RATE * RECORD_SECONDS), samplerate=RATE, channels=CHANNELS, dtype='int16')
+    sd.wait()
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
     print("* Done recording")
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
 
+    # Lưu mảng numpy vào tệp WAV
     wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
     wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setsampwidth(2)  # 2 bytes cho kiểu dữ liệu 'int16'
     wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
+    wf.writeframes(recording_data.tobytes())
     wf.close()
 
